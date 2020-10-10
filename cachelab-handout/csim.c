cd670c5c02fcbe36f70656c0558dc5de23d91787
Yao Xiao <363659894@qq.com>
2020-10-10 20:32:52 +0800

    csim.c and trans.c

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,150 @@
 #include "cachelab.h"
+#include <unistd.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
 
-int main()
+typedef struct {
+    int valid, tag, LRU_cnt;
+} cache_line;
+
+int num_hit, num_miss, num_evict;
+int opt, opt_help, opt_verbose, s, E, b;
+int S;
+cache_line** cache;
+
+void update(unsigned address){
+    unsigned s_addr, t_addr;
+    int max_cnt = 0, replace_e = -1;
+    s_addr = (address>>b)&((-1U)>>(32-s));
+    t_addr = address>>(b+s);
+    for(int i=0;i<E; ++i)
+    {
+        if(cache[s_addr][i].tag==t_addr)
+        {
+            ++num_hit;
+            cache[s_addr][i].LRU_cnt=0;
+            return;
+        }
+    }
+    for (int i=0;i<E; ++i)
+    {
+        if (cache[s_addr][i].valid==0)
+        {
+            ++num_miss;
+            cache[s_addr][i].valid = 1;
+            cache[s_addr][i].tag = t_addr;
+            cache[s_addr][i].LRU_cnt = 0;
+            return;
+        }
+    }
+
+    for (int j=0;j<E;++j)
+    {
+        if (cache[s_addr][j].LRU_cnt > max_cnt)
+        {
+            max_cnt = cache[s_addr][j].LRU_cnt;
+            replace_e = j;
+        }
+    }
+    
+    ++num_miss;
+    ++num_evict;
+    cache[s_addr][replace_e].tag = t_addr;
+    cache[s_addr][replace_e].LRU_cnt = 0;
+    return;
+
+}
+
+
+void update_time(){
+    for(int i=0;i<S;++i)
+    {
+        for(int j=0;j<E;++j)
+        {
+            if(cache[i][j].valid==1) cache[i][j].LRU_cnt += 1;       
+        }
+    }
+}
+
+
+int main(int argc, char** argv)
 {
-    printSummary(0, 0, 0);
+    char filename[100];
+    char buffer[1000];
+    num_hit = num_miss = num_evict = 0;
+    while(-1 != (opt = getopt(argc, argv, "hvs:E:b:t:")))
+    {
+        switch(opt){
+            case 'h':
+                opt_help = 1;
+                break;
+            case 'v':
+                opt_verbose = 1;
+                break;            
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 't':
+                strcpy(filename, optarg);
+                break;
+            default:
+                printf("wrong argument\n");
+                break;
+        }
+    }
+    S = 1<<s;  //2^s
+    cache = (cache_line**)malloc(sizeof(cache_line*) * S);
+    for(int i=0;i<S;++i)
+    {
+        cache[i] = (cache_line*)malloc(sizeof(cache_line) * E);
+    }
+    for(int i=0;i<S;++i)
+    {
+        for(int j=0;j<E;++j)
+        {
+            cache[i][j].valid = 0;
+            cache[i][j].tag = -1;
+            cache[i][j].LRU_cnt = -1;
+        }
+    }
+    FILE* pfile;
+    pfile = fopen(filename, "r");
+    if (pfile==NULL) {
+        fprintf(stderr, "wrong file!\n");
+        exit(-1);
+    }
+    char identifier;
+    unsigned address;
+    int size;
+    while (fgets(buffer, 1000, pfile)) //读数据的方式很关键, fscanf不好用
+    {
+        sscanf(buffer, " %c %xu,%d", &identifier, &address, &size);
+        switch(identifier){
+            case 'M':
+                update(address);
+                update(address);
+                break;
+            case 'L':
+                update(address);
+                break;
+            case 'S':
+                update(address);
+                break;
+        }
+        update_time();
+    }
+    for(int i=0;i<S;++i) free(cache[i]);
+    free(cache);
+    fclose(pfile);
+    printSummary(num_hit, num_miss, num_evict);
     return 0;
 }
--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -22,6 +22,34 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+	int val0,val1,val2,val3,val4,val5,val6,val7;
+	for(int ii=0;ii<N;ii+=8)
+	{
+		for(int jj=0;jj<M;jj+=8)
+		{
+			for(int i=ii;i<ii+8;++i)
+			{
+				int j = jj;
+				val0 = A[i][j];
+				val1 = A[i][j+1];
+				val2 = A[i][j+2];
+				val3 = A[i][j+3];
+				val4 = A[i][j+4];
+				val5 = A[i][j+5];
+				val6 = A[i][j+6];
+				val7 = A[i][j+7];
+				B[j][i] = val0;
+				B[j+1][i] = val1;
+				B[j+2][i] = val2;
+				B[j+3][i] = val3;
+				B[j+4][i] = val4;
+				B[j+5][i] = val5;
+				B[j+6][i] = val6;
+				B[j+7][i] = val7;
+			}
+		}
+	}
+
 }
 
 /* 
@@ -32,20 +60,94 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
-char trans_desc[] = "Simple row-wise scan transpose";
+char trans_desc[] = "blocksize 4 by 4";
 void trans(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
+    int val0, val1, val2, val3;
+	for(int ii=0; ii<N; ii+=4)
+	{
+		for(int jj=0;jj<M; jj+=4)
+		{
+			for(int i=ii;i<ii+4;++i)
+			{
+				int j = jj;
+				val0 = A[i][j];
+				val1 = A[i][j+1];
+				val2 = A[i][j+2];
+				val3 = A[i][j+3];
+				B[j][i] = val0;
+				B[j+1][i] = val1;
+				B[j+2][i] = val2;
+				B[j+3][i] = val3;
+			}
+		}
+	}
 
 }
 
+char trans_desc2[] = "blocksize 16 by 16";
+void trans2(int M, int N, int A[N][M], int B[M][N])
+{
+	int ii, jj, val0, val1, val2, val3, val4, val5,val6, val7;
+	for(ii=0; ii+16<N; ii+=16)
+	{
+		for(jj=0;jj+16<N; jj+=16)
+		{
+			for(int i=ii;i<ii+16;++i)
+			{
+				int j = jj;
+				val0 = A[i][j];
+				val1 = A[i][j+1];
+				val2 = A[i][j+2];
+				val3 = A[i][j+3];
+				val4 = A[i][j+4];
+				val5 = A[i][j+5];
+				val6 = A[i][j+6];
+				val7 = A[i][j+7];
+				B[j][i] = val0;
+				B[j+1][i] = val1;
+				B[j+2][i] = val2;
+				B[j+3][i] = val3;
+				B[j+4][i] = val4;
+				B[j+5][i] = val5;
+				B[j+6][i] = val6;
+				B[j+7][i] = val7;
+				val0 = A[i][j+8];
+				val1 = A[i][j+9];
+				val2 = A[i][j+10];
+				val3 = A[i][j+11];
+				val4 = A[i][j+12];
+				val5 = A[i][j+13];
+				val6 = A[i][j+14];
+				val7 = A[i][j+15];
+				B[j+8][i] = val0;
+				B[j+9][i] = val1;
+				B[j+10][i] = val2;
+				B[j+11][i] = val3;
+				B[j+12][i] = val4;
+				B[j+13][i] = val5;
+				B[j+14][i] = val6;
+				B[j+15][i] = val7;
+
+			}
+		}
+
+	}
+	for(;ii<N;++ii)
+	{
+		for(int j=0;j<M;++j)
+		{
+			B[j][ii] = A[ii][j];
+		}
+	}
+	for(;jj<M;++jj)
+	{
+		for(int i=0;i<N;++i)
+		{
+			B[jj][i] = A[i][jj];
+		}
+	}
+}
 /*
  * registerFunctions - This function registers your transpose
  *     functions with the driver.  At runtime, the driver will
@@ -59,7 +161,8 @@ void registerFunctions()
     registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    registerTransFunction(trans, trans_desc);
+    registerTransFunction(trans2, trans_desc2);	
 
 }
 
